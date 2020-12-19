94dd809f03de520cf3e45fee343512a10a6c124d
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2020-12-19 22:24:11 +0700

    Add files via upload

---
+++ locator.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#Tolonglah jangan direcode
+#aku buat susah susah
+#WhoMHw && Mr.WR.py
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+
+
+
+echo $ku "[$pu+$ku]$pr Siapa nama kamu?"
+read nama
+sleep 0.01
+echo $ku"Loading......!!"
+sleep 3
+echo "             . "
+sleep 0.01
+echo "         .   :   .  "
+sleep 0.01
+echo "     '.   .  :  .   .' "
+sleep 0.01
+echo "  ._   '._.-'''-._.'   _. "
+sleep 0.01
+echo "    '-..'         '..-'  "
+sleep 0.01
+echo " --._ /.==.     .==.\ _.-- "
+sleep 0.01
+echo "     ;/_o__\   /_o__\; "
+sleep 0.01
+echo "-----|'     ) (     '|-----"
+echo "    _: \_) (\_/) (_/ ;_ "
+sleep 0.03
+echo " --'  \  '._.=._.'  /    "
+sleep 0.03
+echo "   _.-''.  '._.'  .''-._ "
+sleep 0.03
+echo "       .''-.(_).-''.    ' "
+sleep 0.03
+echo "     .'   '  :  '   '. "
+sleep 0.01
+echo "        '    :   ' "
+sleep 0.01
+echo "             ' "
+sleep 0.01
+echo 
+echo $or"#####################################"
+sleep 0.03
+echo $me"##         Author : WhoMHw         ##"
+sleep 0.03
+echo $ji"##  https://github.com/WongBiasa   ##"
+sleep 0.03
+echo $or"#####################################"
+
+echo $ku"[$me+$ku]Instagram:https://www.instagram.com/wong_biasa0723/"
+echo $ku"[$me+$ku]Instagram:https://www.instagram.com/siapa_namasaya23/"
+echo $ku"[$me+$ku]Youtube:https://www.youtube.com/channel/UChMIkGetnMB4qq9K5kNwkXA/featured"
+sleep 0.03
+echo 
+
+read -p "Masukkan Ip andres:" Ip
+curl -s https://ip-api.com/$Ip
+
+echo $me"===================="
+echo $ku"=   Terimakasih    ="
+echo $cy"=     Telah        ="
+echo $ij"=  Menggunakan     ="
+echo $bi"=    $nama         ="
+echo $pr"===================="
